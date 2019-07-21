a9b7eeb69a20d52cbe41e9a071690ff88d5fb5e1
izumi-lvgs <haruka.izumi@leverages.jp>
2019-07-21 11:24:10 +0900

    initial commit

---
+++ app/src/main/java/com/leverages/careerticketcafe/views/NewsListActivity.kt
@@ -0,0 +1,13 @@
+package com.leverages.careerticketcafe.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leverages.careerticketcafe.R
+
+class NewsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_list)
+    }
+}
---
+++ app/src/main/java/com/leverages/careerticketcafe/views/SeminarDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.leverages.careerticketcafe.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leverages.careerticketcafe.R
+
+class SeminarDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seminar_detail)
+    }
+}
